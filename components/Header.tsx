00a2a247dc41c99be1085877997c898f558d766f
AbdelrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-11-23 08:31:22 +0200

    ReactTypeWriter Hero Section (BackGroundCircles)

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,7 +14,7 @@ export default function Header({}: Props) {
                 <SocialIcon url="https://linkedin.com/in/abdelrahmangbr" fgColor={"gray"} bgColor={"transparent"}/>
                 <SocialIcon url="https://github.com/AbdElrahmanGbr" fgColor={"gray"} bgColor={"transparent"}/>
                 <SocialIcon url="https://twitter.com/AbdelrahmanGbr4" fgColor={"gray"} bgColor={"transparent"}/>
-                <SocialIcon url="https://discordapp.com/users/AbdElrahman Gbr#5119" fgColor={"gray"} bgColor={"white"}/>
+                <SocialIcon url="https://discordapp.com/users/AbdElrahman Gbr#5119" fgColor={"transparent"} bgColor={"gray"} style={{ height: 30, width: 30 }}/>
             </motion.div>
 
             <motion.div initial={{x: 500, opacity: 0, scale: 0.5}} animate={{x: 0, opacity: 1, scale: 1}} transition={{duration:1.5}} className={"flex flex-row items-center text-gray-300 cursor-pointer"}>
