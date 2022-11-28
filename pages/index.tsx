1596bf135caa63804718120ccb833657f8ccf882
AbdelrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-11-28 17:04:38 +0200

    About Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,28 @@
 import Head from 'next/head'
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import {About} from "../components/About";
 
 export default function Home() {
-  return (
-      <div className={"bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0"}>
-      <Head>
-        <title>Hlbessa's Portfolio</title>
-      </Head>
+    return (<div className={"bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0"}>
+            <Head>
+                <title>Hlbessa's Portfolio</title>
+            </Head>
 
-    <Header/>
-            <section id={"hero"} className={"snap-center"}>
+            <Header/>
+            <section id={"hero"} className={"snap-start"}>
                 <Hero/>
             </section>
 
-          {/*About*/}
+            <section id={"hero"} className={"snap-center"}>
+                <About/>
+            </section>
+            {/*Experience*/}
 
-          {/*Experience*/}
+            {/*Skills*/}
 
-          {/*Skills*/}
+            {/*Projects*/}
 
-          {/*Projects*/}
-
-          {/*Contact Me*/}
-      </div>
-  )
+            {/*Contact Me*/}
+        </div>)
 }
