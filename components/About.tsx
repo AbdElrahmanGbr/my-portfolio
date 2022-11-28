65e8f8ac62b62b624ff4ffd0925d76127344a86d
AbdelrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-11-28 21:06:53 +0200

    Projects/Experience Section

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ import {motion} from 'framer-motion';
 import * as React from 'react';
 
 type Props = {};
-export const About = ({}: Props) => {
+export default function About({}: Props) {
     return (
         <motion.div
             initial={{opacity: 0}} whileInView={{opacity: 1}} transition={{duration: 1.5}}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import Header from "../components/Header";
 import Hero from "../components/Hero";
-import {About} from "../components/About";
+import About from "../components/About";
+import Projects from "../components/Projects";
 
 export default function Home() {
     return (<div className={"bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0"}>
@@ -17,8 +18,10 @@ export default function Home() {
             <section id={"hero"} className={"snap-center"}>
                 <About/>
             </section>
-            {/*Experience*/}
 
+            <section id={"experience"} className={"snap-center"}>
+                <Projects/>
+            </section>
             {/*Skills*/}
 
             {/*Projects*/}
