fffd36a9b0bc6f3de53c57987db67044f1c4a92d
AbdelrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-12-13 06:10:46 +0200

    Added link to ContactMe and footer

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react';
 import {SocialIcon} from "react-social-icons";
 import {motion} from 'framer-motion';
+import Link from "next/link";
 
 type Props = {};
 
@@ -16,11 +17,13 @@ export default function Header({}: Props) {
                 <SocialIcon url="https://twitter.com/AbdelrahmanGbr4" fgColor={"gray"} bgColor={"transparent"}/>
                 <SocialIcon url="https://discordapp.com/users/AbdElrahman Gbr#5119" fgColor={"transparent"} bgColor={"gray"} style={{ height: 30, width: 30 }}/>
             </motion.div>
-
+            <Link href={"#contact"}>
             <motion.div initial={{x: 500, opacity: 0, scale: 0.5}} animate={{x: 0, opacity: 1, scale: 1}} transition={{duration:1.5}} className={"flex flex-row items-center text-gray-300 cursor-pointer"}>
                 <SocialIcon className={"cursor-pointer"} network={"email"} fgColor={"gray"} bgColor={"transparent"}/>
                 <p className={"uppercase hidden md:inline-flex text-sm text-gray-400"}>Get in Touch</p>
             </motion.div>
+            </Link>
+
         </header>
     );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,25 +7,27 @@ import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
 import {NextPage} from "next";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-    return (<div className={"bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"}>
-            <Head>
-                <title>AbdElrahman's Portfolio</title>
-            </Head>
+    return (<div
+        className={"bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"}>
+        <Head>
+            <title>AbdElrahman's Portfolio</title>
+        </Head>
 
-            <Header/>
-            <section id={"hero"} className={"snap-start"}>
-                <Hero/>
-            </section>
+        <Header/>
+        <section id={"hero"} className={"snap-start"}>
+            <Hero/>
+        </section>
 
-            <section id={"hero"} className={"snap-center"}>
-                <About/>
-            </section>
+        <section id={"hero"} className={"snap-center"}>
+            <About/>
+        </section>
 
-            {/*<section id={"experience"} className={"snap-center"}>*/}
-            {/*    <WorkExperience/>*/}
-            {/*</section>*/}
+        {/*<section id={"experience"} className={"snap-center"}>*/}
+        {/*    <WorkExperience/>*/}
+        {/*</section>*/}
 
         <section id={"skills"} className={"snap-start"}>
             <Skills/>
@@ -36,9 +38,17 @@ const Home: NextPage = () => {
         </section>
 
         <section id={"contact"} className={"snap-start"}>
-            <ContactMe />
+            <ContactMe/>
         </section>
-        </div>)
+        <Link href={"#hero"}>
+            <footer className={"sticky bottom-5 w-full cursor-pointer"}>
+                <div className={"flex items-center justify-center"}>
+                    <img className={"h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"}
+                         src="./spud.png" alt=""/>
+                </div>
+            </footer>
+        </Link>
+    </div>)
 }
 
 export default Home;
