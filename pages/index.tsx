3086dae96feabf2954471ec2bbe5a498b0c9be45
AbdelrahmanGbr <abdelrahman.gbr.os@gmail.com>
2022-12-13 06:01:46 +0200

    Added tailwind-scrollbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,10 @@ import About from "../components/About";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
+import {NextPage} from "next";
 
-export default function Home() {
-    return (<div className={"bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0"}>
+const Home: NextPage = () => {
+    return (<div className={"bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80"}>
             <Head>
                 <title>AbdElrahman's Portfolio</title>
             </Head>
@@ -39,3 +40,5 @@ export default function Home() {
         </section>
         </div>)
 }
+
+export default Home;
